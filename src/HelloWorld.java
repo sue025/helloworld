6ca406d68b6cb647257a9f82c7313f886397998d
sooyoungher <sooyoung.her@ybrain.com>
2020-05-25 21:32:54 +0900

    문구수

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {	
 	public static void main(String[] args){
 	System.out.println("HersooyoungWorld");
-	System.out.println("Wow 이거 개발이 잼나는데 해볼만하겠");
+	System.out.println("Wow 이거 개발이 잼나는데 해볼만하겠다");
 	System.out.println("Greatest Sooyoung!!!");
 	}
 }
