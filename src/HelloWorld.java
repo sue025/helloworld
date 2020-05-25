b9e7b64a80d35f4e855b371f5d6545c008acbb90
sooyoungher <sooyoung.her@ybrain.com>
2020-05-25 21:26:54 +0900

    Wow 문구 추가함. 생각보다 개발이 잼난거 같음. 정신만 차리고 순서대로 차근히 하면 문제 없을 것 같은데 한번꼬이면 장난아닐꺼 같

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {	
 	public static void main(String[] args){
 	System.out.println("HersooyoungWorld");
-	System.out.println("Wow it's really fun");
+	System.out.println("Wow 이거 개발이 잼나는데 해볼만하겠다");
 	}
 }
